4a1e2fd640dc07f0c54e0d3cd3058c7d83f227f6
nolanaguirre <nolanaguirre08@gmail.com>
2017-08-01 10:47:44 -0500

    remove eric's projects

--- Eric/checker/enums.py
+++
@@ -1,5 +0,0 @@
-class Type:
-    RED = "red"
-    BLACK= "black"
-    KING = True
-    PAWN = False
--- Eric/checker/piece.py
+++
@@ -1,7 +0,0 @@
-from enums import Type
-class piece:
-    def __init__(self, type, color):
-        self.color = color
-        self.type = type
-    def king(self):
-        self.type = Type.KING
